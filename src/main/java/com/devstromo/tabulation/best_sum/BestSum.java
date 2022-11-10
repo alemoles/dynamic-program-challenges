edf794065e53eddb2e13e5af950cae19ef207210
alemoles <alemolerio@gmail.com>
2022-11-10 20:30:38 -0300

    best sum tabulation

--- src/main/java/com/devstromo/tabulation/best_sum/BestSum.java
+++ src/main/java/com/devstromo/tabulation/best_sum/BestSum.java
@@ -1,7 +1,31 @@
 package com.devstromo.tabulation.best_sum;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BestSum {
-    public int[] solve(int target, int[] numbers) {
-        return new int[0];
+    public List<Integer> solve(int targetSum, int[] numbers) {
+        var table = new ArrayList<List<Integer>>(targetSum + 1);
+        table.add(0, new ArrayList<>());
+        for (int i = 1; i <= targetSum; i++) {
+            table.add(i, null);
+        }
+        for (int i = 0; i < targetSum; i++) {
+            if (table.get(i) != null) {
+                for (var number : numbers) {
+                    if (i + number <= targetSum) {
+                        var combination = new ArrayList<>(table.get(i));
+                        combination.add(number);
+                        if (table.get(i + number) == null || (table.get(i + number) != null && table.get(i + number)
+                            .size() > combination.size())) {
+                            table.set(i + number, combination);
+                        }
+                    }
+                }
+            }
+        }
+        return table.get(targetSum) != null && table.get(targetSum)
+            .stream()
+            .reduce(0, Integer::sum) == targetSum ? table.get(targetSum) : null;
     }
 }
--- src/test/java/com/devstromo/tabulation/best_sum/BestSumTest.java
+++ src/test/java/com/devstromo/tabulation/best_sum/BestSumTest.java
@@ -1,6 +1,9 @@
 package com.devstromo.tabulation.best_sum;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,9 +12,10 @@ class BestSumTest {
 
     @Test
     public void testHowSum() {
-        assertArrayEquals(new int[] { 7 }, bestSum.solve(7, new int[] { 5, 3, 4, 7 }));
-        assertArrayEquals(new int[] { 5, 3 }, bestSum.solve(8, new int[] { 2, 3, 5 }));// [3,5]
-        assertArrayEquals(new int[] { 4, 4 }, bestSum.solve(8, new int[] { 1, 4, 5 }));
-        assertArrayEquals(new int[] { 25, 25, 25, 25 }, bestSum.solve(100, new int[] { 1, 2, 5, 25 }));
+        assertEquals(List.of(7), bestSum.solve(7, new int[] { 5, 3, 4, 7 }));
+        assertEquals(List.of(3, 5), bestSum.solve(8, new int[] { 2, 3, 5 }));// [3,5]
+        assertEquals(List.of(4, 4), bestSum.solve(8, new int[] { 1, 4, 5 }));
+        assertEquals(List.of(25, 25, 25, 25), bestSum.solve(100, new int[] { 1, 2, 5, 25 }));
+        assertEquals(Collections.emptyList(), bestSum.solve(0, new int[] { 1, 2, 5, 25 }));
     }
 }
